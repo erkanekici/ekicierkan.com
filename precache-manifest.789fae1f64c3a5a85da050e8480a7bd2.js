9e3e04a94c3e6bbf408d9d33da93b571a10ccc17
erkanekici <erkan>
2021-02-05 23:25:07 +0300

    Updates

---
+++ precache-manifest.789fae1f64c3a5a85da050e8480a7bd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84284fc6478f1965ca9397fe18e19cf3",
+    "url": "/static/media/motto.84284fc6.jpg"
+  },
+  {
+    "revision": "b683d197d5047123c1e3d244c445b24c",
+    "url": "/static/media/backgroundSound.b683d197.mp3"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c71b30cbe968605cc46c",
+    "url": "/static/js/main.c71b30cb.chunk.js"
+  },
+  {
+    "revision": "ca2dc8bbed21cae914eb",
+    "url": "/static/js/2.ca2dc8bb.chunk.js"
+  },
+  {
+    "revision": "c71b30cbe968605cc46c",
+    "url": "/static/css/main.5ed2699e.chunk.css"
+  },
+  {
+    "revision": "50f1f02a8754d7bed95945552b92269f",
+    "url": "/index.html"
+  }
+];
